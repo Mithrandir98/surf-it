e9f4d3b7569d13d2868c3b44f2aeecf31010dc7f
Ruthwik H Param <aspire1998@gmail.com>
2020-09-05 13:45:29 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const postsRouter = require("./routes/posts");
+const reviewsRouter = require("./routes/reviews");
 
 const app = express();
 
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
+app.use("/posts/:id/reviews", reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -36,24 +36,24 @@ router.put("/profile/:user_id", (req, res, next) => {
   res.send("PUT /profile/:user_id");
 });
 
-/* GET /forgot-password */
-router.get("/forgot-pw", (req, res, next) => {
-  res.send("GET /forgot-pw");
+/* GET /forgot */
+router.get("/forgot", (req, res, next) => {
+  res.send("GET /forgot");
 });
 
-/* PUT /forgot-password */
-router.put("/forgot-pw", (req, res, next) => {
-  res.send("PUT /forgot-pw");
+/* PUT /forgot */
+router.put("/forgot", (req, res, next) => {
+  res.send("PUT /forgot");
 });
 
-/* GET /reset-password */
-router.get("/reset-pw/:token", (req, res, next) => {
-  res.send("GET /reset-pw/:token");
+/* GET /reset */
+router.get("/reset/:token", (req, res, next) => {
+  res.send("GET /reset/:token");
 });
 
-/* PUT /reset-password */
-router.put("/reset-pw/:token", (req, res, next) => {
-  res.send("PUT /reset-pw/:token");
+/* PUT /reset */
+router.put("/reset/:token", (req, res, next) => {
+  res.send("PUT /reset/:token");
 });
 
 module.exports = router;
