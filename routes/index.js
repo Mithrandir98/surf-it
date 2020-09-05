b8772fecf9757c81d667cbd828f155bd29ee9756
Ruthwik H Param <aspire1998@gmail.com>
2020-09-05 13:44:33 +0700

    Added user routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index", { title: "Surf It - Home" });
+});
+
+/* GET /register */
+router.get("/register", (req, res, next) => {
+  res.send("GET /register");
+});
+
+/* POST /register */
+router.post("/register", (req, res, next) => {
+  res.send("POST /register");
+});
+
+/* GET /login */
+router.get("/login", (req, res, next) => {
+  res.send("GET /login");
+});
+
+/* POST /login */
+router.post("/login", (req, res, next) => {
+  res.send("POST /login");
+});
+
+/* GET /profile */
+router.get("/profile", (req, res, next) => {
+  res.send("GET /profile");
+});
+
+/* PUT /profile/:user_id */
+router.put("/profile/:user_id", (req, res, next) => {
+  res.send("PUT /profile/:user_id");
+});
+
+/* GET /forgot-password */
+router.get("/forgot-pw", (req, res, next) => {
+  res.send("GET /forgot-pw");
+});
+
+/* PUT /forgot-password */
+router.put("/forgot-pw", (req, res, next) => {
+  res.send("PUT /forgot-pw");
+});
+
+/* GET /reset-password */
+router.get("/reset-pw/:token", (req, res, next) => {
+  res.send("GET /reset-pw/:token");
+});
+
+/* PUT /reset-password */
+router.put("/reset-pw/:token", (req, res, next) => {
+  res.send("PUT /reset-pw/:token");
 });
 
 module.exports = router;
