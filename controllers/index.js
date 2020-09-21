74092a943431e27ada2333f11b3ab60a94f851a9
Ruthwik H Param <aspire1998@gmail.com>
2020-09-21 12:11:38 +0700

    UserAuth

---
+++ controllers/index.js
@@ -0,0 +1,17 @@
+const User = require('../models/user');
+
+module.exports = {
+    postRegister(req, res, next) {
+        console.log('registering user');
+  User.register(new User({username: req.body.username}), req.body.password, (err) => {
+    if (err) {
+      console.log('error while user register!', err);
+      return next(err);
+    }
+
+    console.log('user registered!');
+
+    res.redirect('/');
+  });
+    }
+}
